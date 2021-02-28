ffe67f45033102a1be4e77e9f4e95f0892adbd16
Srujith Madadi <srujith.madadi@walmart.com>
2021-02-28 16:56:51 -0600

    changes to api end points

--- src/Store/ActionCreators/JobActions.js
+++ src/Store/ActionCreators/JobActions.js
@@ -3,10 +3,21 @@ import * as Actions from "Store/Actions/Actions"
 
 export const getJobs=(searchObject)=>{
 	return dispatch=>{
-		return Axios.get("/getJobs",{
-			params:{...searchObject}
-		}).then((response)=>{
-			dispatch({type:Actions.GETJOBS,jobs:response.data})
+		return Axios.get("https://jobs.github.com/positions.json").then((response)=>{
+			console.log(response,"response")
+			let jobs=JSON.parse(response)
+			jobs=jobs.map((job)=>{
+				return {
+					time:Math.abs(Math.round((new Date().getTime()-new Date(job.created_at).getTime())/(1000*60*60)))+"h ago",
+					id:job.id,
+					type:job.type,
+					title:job.title,
+					company:job.company,
+					location:job.location,
+					logo:job.company_logo
+				}
+			})
+			dispatch({type:Actions.GETJOBS,jobs:jobs})
 		})
 	}
 }
