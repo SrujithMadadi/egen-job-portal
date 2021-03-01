60012eeb03ad4c57987cf23dbf3475db2f6dda93
Srujith Madadi <srujith.madadi@walmart.com>
2021-02-28 19:54:11 -0600

    css changes for mobile

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import {BrowserRouter,Switch,Route} from "react-router-dom";
 import Jobs from "Containers/Jobs/Jobs";
+// import MobileJobs from "Containers/JobsMobile/Jobs"
 import JobDetails from "Containers/JobDetails/JobDetails"
 import { StylesProvider } from '@material-ui/core/styles';
+// import { useMediaQuery } from 'react-responsive';
 import Header from "Components/Header/Header";
 
 const App=()=>{
+
+	// const isMobile=useMediaQuery({
+	// 	query: '(max-width: 768px)'
+	// })
+
 	return (
 		<StylesProvider injectFirst>
 			<BrowserRouter>
---
+++ src/Containers/JobsMobile/Jobs.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Jobs=()=>{
+	return (
+		<div>This is mobile device</div>
+	)
+}
+
+export default Jobs;
